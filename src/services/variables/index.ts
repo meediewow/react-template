f1111b5d0a20d8160aaed249987b6cce11f92062
Mikhail Didevich <mike_mojet@live.com>
2021-04-02 17:06:44 +0300

    updated projectName & readme

--- src/services/variables/index.ts
+++ src/services/variables/index.ts
@@ -3,7 +3,7 @@ import * as routePathes from "services/variables/routes";
 class VariableClass {
     public API_URL = "https://api.unsplash.com/";
     public BACK_URL = "https://unsplash.com/";
-    public PROJECT_NAME = "JAZZPIXELS TEST";
+    public PROJECT_NAME = "React Template";
     public REDIRECT_URL = `http://localhost:8080${routePathes.LOGIN}`;
 }
 
