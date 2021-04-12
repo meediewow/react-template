fcf13f56357b620a8c90019e129d159f9499b2bc
Mikhail Didevich <mike_mojet@live.com>
2021-04-12 09:19:14 +0300

    fixed redirect

--- src/hooks/use-redirect.ts
+++ src/hooks/use-redirect.ts
@@ -3,17 +3,20 @@ import { useHistory } from "react-router-dom";
 import * as routePathes from "services/variables/routes";
 import { isAuthorized } from "services/helpers/user/authorize";
 
-interface IProps {
-    redirectOnHome?: boolean;
-}
-
-export const useRedirect = (props: IProps = {}): void => {
+export const useRedirect = (): void => {
     const history = useHistory();
     useEffect(() => {
         if (!isAuthorized()) {
             history.push(routePathes.LOGIN);
-        } else if (props.redirectOnHome) {
+        }
+    }, [history]);
+};
+
+export const useAuthorizedRedirect = (): void => {
+    const history = useHistory();
+    useEffect(() => {
+        if (isAuthorized()) {
             history.push(routePathes.HOME);
         }
-    }, [history, props.redirectOnHome]);
+    }, [history]);
 };
